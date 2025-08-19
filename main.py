13b4d6b0c17b0a3c75a1396750d309619074ba98
Kilian <kischoenhaar@gmail.com>
2025-08-19 22:08:02 +0200

    add /hello with name, lang...

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 from fastapi import FastAPI
+from typing import Optional
+from typing import List
+from fastapi import Query
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello FastAPI"}
+
+@app.get("/hello/{name}")
+def hello_name(name: str,
+               lang: Optional[str] = None,
+               upper: Optional[bool] = False,
+               style: Optional[str] = "casual",
+               times: Optional[int] = 1
+               ):
+    if lang == "de":
+        if style == "formal":
+            message = f"Guten Tag {name}"
+        else:
+            message = f"Hallo {name}"
+    elif lang == "en":
+        message = f"Hello {name}"
+    elif lang == "fr":
+        message = f"Bonjour {name}"
+    else:
+        message = f"Hello {name}"
+
+    if upper:
+        message = message.upper()
+
+    # messages = []
+    # for _ in range(times):
+    #     messages.append(message)
+    messages = [message for _ in range(times)]
+
+    return {"message": messages}
+
+@app.get("/tags")
+def get_tags(tag: List[str] = Query([])):
+    return {"tags": tag}
